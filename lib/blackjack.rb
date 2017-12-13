d75b2ef8c9668d7e7c61503db6f643184d79b390
Katy Carr <khcarr9@gmail.com>
2017-12-13 19:26:08 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -43,11 +43,12 @@ def hit?(card_total)
   input = get_user_input
   if input == "h"
     new_card = deal_card
+    card_total += new_card
   elsif input != "s" || input != "h"
     invalid_command
     prompt_user
   end
-  card_total + new_card
+  card_total
 end
 
 def invalid_command
